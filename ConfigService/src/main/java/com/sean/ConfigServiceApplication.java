0a90845a17aad84432caae228cbe3ef6d8123eca
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-08 23:29:22 -0600

    configured config server, eureka, blog-service

--- ConfigService/src/main/java/com/sean/ConfigServiceApplication.java
+++ ConfigService/src/main/java/com/sean/ConfigServiceApplication.java
@@ -2,9 +2,12 @@ package com.sean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
+	// Run with profile 'native'
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);

3af8c270064c32164a270d5bcd52cf073a77a55f
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-09 21:56:23 -0600

    Added ApiGateway - WIP

--- BlogService/src/main/java/com/sean/BlogController.java
+++ BlogService/src/main/java/com/sean/BlogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/blogs")
+@RequestMapping("/blogs/api/v1/blogs")
 public class BlogController {
 
     @GetMapping

829dded2522fe24e2c546275fe5f06de2ce259f4
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-15 23:16:05 -0600

    Updated BlogWriterService kafka listener to use Message wrapper

--- BlogWriterService/src/main/java/com/sean/listeners/BlogListener.java
+++ BlogWriterService/src/main/java/com/sean/listeners/BlogListener.java
@@ -5,6 +5,7 @@ import com.sean.repositories.BlogRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,10 +16,10 @@ public class BlogListener {
     private BlogRepository blogRepository;
 
     @Bean
-    public java.util.function.Consumer<Blog> saveNewBlog() {
-        return blog -> {
-            log.info("Saving new blog: {}", blog);
-            blogRepository.save(blog);
+    public java.util.function.Consumer<Message<Blog>> saveNewBlog() {
+        return blogMessage -> {
+            log.info("Saving new blog: {}", blogMessage);
+            blogRepository.save(blogMessage.getPayload());
         };
     }
 }

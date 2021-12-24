5de5e49165302119e81bcd19aed831e0bd1f4b97
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-23 18:49:12 -0600

    Fixed nullPointer in BlogWriterService

--- BlogWriterService/src/main/java/com/sean/services/BlogService.java
+++ BlogWriterService/src/main/java/com/sean/services/BlogService.java
@@ -2,11 +2,13 @@ package com.sean.services;
 
 import com.sean.models.Blog;
 import com.sean.repositories.BlogRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class BlogService {
 
     private BlogRepository blogRepository;

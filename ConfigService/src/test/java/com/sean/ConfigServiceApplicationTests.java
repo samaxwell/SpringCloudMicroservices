bf98ee550aee4767bad02fc5e0664c3fbb64ec22
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-23 22:09:53 -0600

    Reversed eureka dependency on config service

--- ConfigService/src/test/java/com/sean/ConfigServiceApplicationTests.java
+++ ConfigService/src/test/java/com/sean/ConfigServiceApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 /* Use native profile to look for configs on disk */
-@ActiveProfiles("native")
+//@ActiveProfiles("native")
+@ActiveProfiles("test")
 @SpringBootTest
 class ConfigServiceApplicationTests {
 
